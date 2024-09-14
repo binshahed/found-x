95fe55611702d265629251f0d38a635ca896feea
Shahed <151946873+shahed-emsl@users.noreply.github.com>
2024-09-14 17:20:40 +0600

    first

---
+++ FoundX-client/src/app/(main)/page.tsx
@@ -0,0 +1,9 @@
+import { title, subtitle } from "@/src/components/primitives";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <h1>home page</h1>
+    </section>
+  );
+}
